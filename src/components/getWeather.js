d80aaf1a801a53ea07fbc3e18c18170e4d89e5ce
Charlie Aleck (they/them) <aleckcharlie@gmail.com>
2022-02-10 11:07:51 -0600

    added styles, form and button

--- src/components/getWeather.js
+++ src/components/getWeather.js
@@ -10,8 +10,19 @@ export default function getWeather() {
   return (
     //basic input form to search by city
     <div className="App">
-      <h1>Weather App</h1>
-      
+      <h1 className="title">Weather App</h1>
+      <form className="form">
+      <label className="label" htmlFor="query">
+      <p>Search by City</p>
+      </label>
+      <input 
+        className="input" 
+        type="text" 
+        name="query place"
+        placeholder="i.e. Seattle">
+      </input>
+      <button className="button">Search</button>
+      </form>
  
     <CityCard />
     </div>
