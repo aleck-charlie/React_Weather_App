4954868620b91275678180888fce0ce939d5d742
Charlie Aleck (they/them) <aleckcharlie@gmail.com>
2022-02-10 10:06:43 -0600

    adding simple pseudocode

--- src/components/cityCard.js
+++ src/components/cityCard.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 export default function CityCard(){
+  //pass props to cards
   return (
+    //card to show weather
     <>
     </>
   );
--- src/components/getWeather.js
+++ src/components/getWeather.js
@@ -3,10 +3,16 @@ import "../styles.css";
 import CityCard from "./cityCard";
 
 export default function getWeather() {
+  // set up useState
+
+  //fetch data 
+
   return (
+    //basic input form to search by city
     <div className="App">
       <h1>Weather App</h1>
-
+      
+ 
     <CityCard />
     </div>
   );
